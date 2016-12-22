6f69883767edac2dec06e33ad5b10ff9ed26d083
Kang subin <lyricia01@gmail.com>
2016-12-22 21:51:03 +0900

    update
    Fix Install.exe bug

--- Project/Main.py
+++ Project/Main.py
@@ -1,5 +1,3 @@
-from MainFramework import *
-
 import platform
 import os
 
@@ -7,6 +5,9 @@ if platform.architecture()[0]=='32bit':
     os.environ["PYSDL2_DLL_PATH"] = "./SDL2/x86"
 else:
     os.environ["PYSDL2_DLL_PATH"] = "./SDL2/x64"
+
+
+from MainFramework import *
 
 Game=Framework()
 
