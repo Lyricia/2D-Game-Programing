83559059f76c34f0d3c161353cf44242224d6b26
Kang subin <lyricia01@gmail.com>
2016-12-22 20:32:59 +0900

    update fix bug

--- Project/Setup.py
+++ Project/Setup.py
@@ -1,13 +1,14 @@
 import os
 import cx_Freeze
 
-os.environ['TCL_LIBRARY'] = 'C:\\Users\\tkdql\\AppData\\Local\\Programs\\Python\\Python35\\tcl\\tcl8.6'
-os.environ['TK_LIBRARY'] = 'C:\\Users\\tkdql\\AppData\\Local\\Programs\\Python\\Python35\\tcl\\tk8.6'
+os.environ['TCL_LIBRARY'] = 'C:\\Users\\Admin\\AppData\\Local\\Programs\\Python\\Python35\\tcl\\tcl8.6'
+os.environ['TK_LIBRARY'] = 'C:\\Users\\Admin\\AppData\\Local\\Programs\\Python\\Python35\\tcl\\tk8.6'
 executables = [cx_Freeze.Executable(script='Main.py')]
 
 cx_Freeze.setup(
     name='Drop Da Beat!',
-    options={'build_exe': {'packages':['pico2d'],
-                           'include_files':['./Resources/']}},
+    version = '1.1',
+    options={'build_exe': {'packages':['pico2d', 'sdl2'],
+                           'include_files':['./Resources/','./SDL2/' ]}},
     executables = executables
 )
